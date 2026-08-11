0fabf846f22fe0da9c1f56a18cd1506c1b022fd8
Samir Seth <samirkseth@gmail.com>
2026-08-11 05:44:43 +0530

    differential geometry

---
+++ 07. differential-geometry/sources.py
@@ -0,0 +1,27 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.19.5
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# [Dialect - Differential Geometry](https://www.youtube.com/playlist?list=PL__fY7tXwodm9pFqWGrkjIvJMOedSYIti) - fantastic explanations of complex topics.
+#
+# [Physics for Students - Learning Differential Geometry](https://www.youtube.com/watch?v=9qHeuwQeiGY)
+#
+# [Differential Geometry - Claudio Arezzo](https://www.youtube.com/playlist?list=PLLq_gUfXAnkl5JArcktbOrIUeR5rra-Gz)
+#
+# [Discrete Differential Geometry ](https://brickisland.net/DDGSpring2022/)
+
+# %% [markdown]
+# https://www.celsiuselements.com/math/differential-geometry-video-lectures/
+
+# %%
