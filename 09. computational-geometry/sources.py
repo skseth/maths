d4f5d0205527196da811f222c70114e2b08fc2e9
Samir Seth <samirkseth@gmail.com>
2026-08-11 05:46:08 +0530

    computational geometry

---
+++ 09. computational-geometry/sources.py
@@ -0,0 +1,17 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.19.5
+# ---
+
+# %% [markdown]
+# [Computational Geometry in Python - from Theory to Implementation](https://www.toptal.com/developers/python/computational-geometry-in-python-from-theory-to-implementation)
+#
+# [Computation Geometry in Python](https://deeplearning.lipingyang.org/wp-content/uploads/2019/03/Computational-Geometry-in-Python.pdf)
+
+# %% [markdown]
+#
